578b1d8980f5fa3556cfc7983f4b382eedf80990
Jacob Brewer <Jacob.brewer@unity3d.com>
2023-02-24 07:59:14 +0000

    Not logging the message type on receive

--- src/discord/listening.go
+++ src/discord/listening.go
@@ -6,13 +6,12 @@ func (s *Session) listen() {
 	log.Println("listening to new messages")
 
 	for {
-		messageType, message, err := s.connection.ReadMessage()
+		_, message, err := s.connection.ReadMessage()
 		if err != nil {
 			log.Println("reading websocket message:", err)
 			return
 		}
 
-		log.Println(messageType)
 		log.Println(string(message))
 
 		select {
