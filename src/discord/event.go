98c0813dbc9f18782f8c5ea977cfc28455419e63
Jacob Brewer <Jacob.brewer@bthree.uk>
2023-02-25 17:13:35 +0000

    [discord-connection] Adding comment

--- src/discord/event.go
+++ src/discord/event.go
@@ -2,6 +2,8 @@ package discord
 
 import "encoding/json"
 
+// Event is what all events should inherit from. The structure of the events will be
+// defined under the RawData part of this struct
 type Event struct {
 	Opcode   Opcode          `json:"op"`
 	Sequence int64           `json:"s"`
